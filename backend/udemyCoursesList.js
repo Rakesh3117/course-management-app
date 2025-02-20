c60dedd2fbfeacc97f97b506efe7d678a7257307
Rakesh3117 <annavarapurakesh190799@gmail.com>
2025-02-21 00:01:45 +0530

    Removed node_modules from Git tracking

---
+++ backend/udemyCoursesList.js
@@ -0,0 +1,248 @@
+export const TrustedCompanies = {
+  data: {
+    dynamicWebContent: {
+      heading:
+        "Trusted by over 16,000 companies and millions of learners around the world",
+      contents: [
+        {
+          image: {
+            url: "https://cms-images.udemycdn.com/content/tqevknj7om/svg/volkswagen_logo.svg?position=c&quality=80&x.app=portals",
+            altText: "Volkswagen logo gray and white",
+          },
+        },
+        {
+          image: {
+            url: "https://cms-images.udemycdn.com/content/2gevcc0kxt/svg/samsung_logo.svg?position=c&quality=80&x.app=portals",
+            altText: "Samsung logo gray and white",
+          },
+        },
+        {
+          image: {
+            url: "https://cms-images.udemycdn.com/content/mueb2ve09x/svg/cisco_logo.svg?position=c&quality=80&x.app=portals",
+            altText: "Cisco logo gray and white",
+          },
+        },
+        {
+          image: {
+            url: "https://cms-images.udemycdn.com/content/ryaowrcjb2/svg/vimeo_logo_resized-2.svg?position=c&quality=80&x.app=portals",
+            altText: "Vimeo logo gray and white",
+          },
+        },
+        {
+          image: {
+            url: "https://cms-images.udemycdn.com/content/bthyo156te/svg/procter_gamble_logo.svg?position=c&quality=80&x.app=portals",
+            altText: "Procter & Gamble logo gray and white",
+          },
+        },
+        {
+          image: {
+            url: "https://cms-images.udemycdn.com/content/luqe0d6mx2/svg/hewlett_packard_enterprise_logo.svg?position=c&quality=80&x.app=portals",
+            altText: "Hewlett Packard logo gray and white",
+          },
+        },
+        {
+          image: {
+            url: "https://cms-images.udemycdn.com/content/siaewwmkch/svg/citi_logo.svg?position=c&quality=80&x.app=portals",
+            altText: "Citi logo gray and white",
+          },
+        },
+        {
+          image: {
+            url: "https://cms-images.udemycdn.com/content/swmv0okrlh/svg/ericsson_logo.svg?position=c&quality=80&x.app=portals",
+            altText: "Ericsson logo gray and white",
+          },
+        },
+      ],
+    },
+  },
+};
+
+export const Categories = {
+  _class: "course_category",
+  id: 288,
+  stats: [
+    {
+      sd_tag: {
+        id: 7380,
+        title: "Python",
+        display_name: "Python",
+        url: "/topic/python/",
+        icon_class: "udi udi-line-star",
+        type: "topic",
+        _class: "course_label",
+        status: 1,
+      },
+      inner_sd_tag: {
+        id: 12,
+        title: "Programming Languages",
+        title_cleaned: "programming-languages",
+        url: "/courses/development/programming-languages/",
+        icon_class: "udi udi-development",
+        type: "subcategory",
+        channel_id: null,
+        _class: "course_subcategory",
+      },
+    },
+    {
+      sd_tag: {
+        id: 8322,
+        title: "Web Development",
+        display_name: "Web Development",
+        url: "/topic/web-development/",
+        icon_class: "udi udi-line-star",
+        type: "topic",
+        _class: "course_label",
+        status: 1,
+      },
+      inner_sd_tag: {
+        id: 8,
+        title: "Web Development",
+        title_cleaned: "web-development",
+        url: "/courses/development/web-development/",
+        icon_class: "udi udi-development",
+        type: "subcategory",
+        channel_id: null,
+        _class: "course_subcategory",
+      },
+    },
+    {
+      sd_tag: {
+        id: 5336,
+        title: "Data Science",
+        display_name: "Data Science",
+        url: "/topic/data-science/",
+        icon_class: "udi udi-line-star",
+        type: "topic",
+        _class: "course_label",
+        status: 1,
+      },
+      inner_sd_tag: {
+        id: 558,
+        title: "Data Science",
+        title_cleaned: "data-science",
+        url: "/courses/development/data-science/",
+        icon_class: "udi udi-development",
+        type: "subcategory",
+        channel_id: null,
+        _class: "course_subcategory",
+      },
+    },
+    {
+      sd_tag: {
+        id: 6368,
+        title: "JavaScript",
+        display_name: "JavaScript",
+        url: "/topic/javascript/",
+        icon_class: "udi udi-line-star",
+        type: "topic",
+        _class: "course_label",
+        status: 1,
+      },
+      inner_sd_tag: {
+        id: 8,
+        title: "Web Development",
+        title_cleaned: "web-development",
+        url: "/courses/development/web-development/",
+        icon_class: "udi udi-development",
+        type: "subcategory",
+        channel_id: null,
+        _class: "course_subcategory",
+      },
+    },
+  ],
+};
+
+export const Categories_two = {
+  _class: "course_category",
+  id: 268,
+  stats: [
+    {
+      sd_tag: {
+        id: 7224,
+        title: "PMI Project Management Professional (PMP)",
+        display_name: "PMI Project Management Professional (PMP)",
+        url: "/topic/pmp/",
+        icon_class: "udi udi-line-star",
+        type: "topic",
+        _class: "course_label",
+        status: 1,
+      },
+      inner_sd_tag: {
+        id: 38,
+        title: "Project Management",
+        title_cleaned: "project-management",
+        url: "/courses/business/project-management/",
+        icon_class: "udi udi-business",
+        type: "subcategory",
+        channel_id: null,
+        _class: "course_subcategory",
+      },
+    },
+    {
+      sd_tag: {
+        id: 6740,
+        title: "Microsoft Power BI",
+        display_name: "Microsoft Power BI",
+        url: "/topic/microsoft-power-bi/",
+        icon_class: "udi udi-line-star",
+        type: "topic",
+        _class: "course_label",
+        status: 1,
+      },
+      inner_sd_tag: {
+        id: 44,
+        title: "Business Analytics & Intelligence",
+        title_cleaned: "analytics-and-intelligence",
+        url: "/courses/business/analytics-and-intelligence/",
+        icon_class: "udi udi-business",
+        type: "subcategory",
+        channel_id: null,
+        _class: "course_subcategory",
+      },
+    },
+    {
+      sd_tag: {
+        id: 7336,
+        title: "Project Management",
+        display_name: "Project Management",
+        url: "/topic/project-management/",
+        icon_class: "udi udi-line-star",
+        type: "topic",
+        _class: "course_label",
+        status: 1,
+      },
+      inner_sd_tag: {
+        id: 38,
+        title: "Project Management",
+        title_cleaned: "project-management",
+        url: "/courses/business/project-management/",
+        icon_class: "udi udi-business",
+        type: "subcategory",
+        channel_id: null,
+        _class: "course_subcategory",
+      },
+    },
+    {
+      sd_tag: {
+        id: 7862,
+        title: "SQL",
+        display_name: "SQL",
+        url: "/topic/sql/",
+        icon_class: "udi udi-line-star",
+        type: "topic",
+        _class: "course_label",
+        status: 1,
+      },
+      inner_sd_tag: {
+        id: 44,
+        title: "Business Analytics & Intelligence",
+        title_cleaned: "analytics-and-intelligence",
+        url: "/courses/business/analytics-and-intelligence/",
+        icon_class: "udi udi-business",
+        type: "subcategory",
+        channel_id: null,
+        _class: "course_subcategory",
+      },
+    },
+  ],
+};
